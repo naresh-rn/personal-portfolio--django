2028b650ad8964b8ad793341c0cc8dae8f2c856f
naresh-rn <naresh.ravic@gmail.com>
2024-01-03 23:01:11 +0530

    set true

---
+++ mywebsite/local_settings.py
@@ -0,0 +1,13 @@
+import os
+
+from click import Path
+
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
