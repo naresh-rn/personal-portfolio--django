b0509eac2f24e27265acf97764ade3189efc600f
naresh-rn <naresh.ravic@gmail.com>
2024-01-03 15:14:51 +0530

    set true

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*!ekwavk6xv1^p6@3rw0jja!#uxmt+-ml3y42y__8j*ps-6brm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
